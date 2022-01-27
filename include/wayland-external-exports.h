247335d5d451bc56af08781c70f8bc96926cbf23
Erik Kurzinger <ekurzinger@nvidia.com>
2022-01-26 16:48:19 -0800

    bump version to 1.1.10

--- include/wayland-external-exports.h
+++ include/wayland-external-exports.h
@@ -53,7 +53,7 @@
  #define WAYLAND_EXTERNAL_VERSION_MINOR                      0
 #endif
 
-#define WAYLAND_EXTERNAL_VERSION_MICRO                       9
+#define WAYLAND_EXTERNAL_VERSION_MICRO                       10
 
 
 #define EGL_EXTERNAL_PLATFORM_VERSION_MAJOR WAYLAND_EXTERNAL_VERSION_MAJOR
