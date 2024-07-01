NVIDIA/egl-wayland

commit 63e82b06fbc09041fdc5fa86c3d0c297b2887781

wayland-egl: avoid unnecessary commit
Earlier in wlEglCreateFrameSync() API, a wl_surface_commit() was called
after a frame callback was created with wl_surface_frame(). When the frame
is actually committed with wlEglSendDamageEvent()
(either directly from SwapBuffers / from damage thread), another
wl_surface_commit() is called. So for each SwapBuffers() call, two
wl_surface_commit() are getting submitted which may result in some
performance issues as weston endup in scheduling two repaints for each
SwapBuffers() call.

To avoid this, creating frame callback only before the actual
wl_surface_commit() call (either from SwapBuffers / damage thread).

But with this update the app thread need to wait for the damage_thread to
commit the surface and generate a frame sync, introduced a conditional
variable and a mutex for this purpose.

src/wayland-eglsurface.c
@@ -127,22 +127,6 @@ void wlEglCreateFrameSync(WlEglSurface *surface)
                                      &throttle_listener, surface) == -1) {
             return;
         }
-
-        /* After a window resize, the compositor has to be
-         * updated with the new buffer's geometry. However, we
-         * won't have updated geometry information until the
-         * underlying buffer is attached. A surface attach
-         * may be deferred to a later time in some situations
-         * (e.g. FIFO_SYNCHRONOUS + damage thread).
-         *
-         * Therefore, the surface_commit done from here would
-         * use outdated geometry information if the buffer is
-         * not attached, which would make xdg-shell fail with
-         * error. To avoid this, skip the surface commit here
-         * if the surface attach is not yet done. */
-        if (surface->ctx.isAttached) {
-            wl_surface_commit(surface->wlSurface);
-        }
     }
 }
 
@@ -258,9 +242,18 @@ damage_thread(void *args)
                     data->egl.streamFlush(display->devDpy->eglDisplay,
                                           surface->ctx.eglStream);
                 }
+
+                pthread_mutex_lock(&surface->mutexFrameSync);
+
+                wlEglCreateFrameSync(surface);
+
                 ok = wlEglSendDamageEvent(surface, queue);
                 surface->ctx.framesProcessed++;
-             }
+
+                pthread_cond_signal(&surface->condFrameSync);
+
+                pthread_mutex_unlock(&surface->mutexFrameSync);
+            }
 
             // Otherwise, wait for sync to trigger
             else {
@@ -1577,6 +1570,18 @@ WlEglSurface *wlEglInitializeSurfaceExport(EGLDisplay dpy,
         goto fail;
     }
 
+    if (!wlEglInitializeMutex(&surface->mutexFrameSync)) {
+        pthread_mutex_unlock(&display->mutex);
+        wlEglReleaseDisplay(display);
+        return EGL_FALSE;
+    }
+
+    if (pthread_cond_init(&surface->condFrameSync, NULL)) {
+        pthread_mutex_unlock(&display->mutex);
+        wlEglReleaseDisplay(display);
+        return EGL_FALSE;
+    }
+
     if (create_surface_context(surface) != EGL_SUCCESS) {
         wl_event_queue_destroy(surface->wlEventQueue);
         goto fail;
@@ -1816,6 +1821,11 @@ void wlEglSurfaceUnref(WlEglSurface *surface)
 
     wlEglMutexDestroy(&surface->mutexLock);
 
+    if (!surface->ctx.isOffscreen) {
+        wlEglMutexDestroy(&surface->mutexFrameSync);
+        pthread_cond_destroy(&surface->condFrameSync);
+    }
+
     for (i = 0; i < surface->ctx.numStreamImages; i++) {
         wlEglMutexDestroy(&surface->ctx.streamImages[i]->mutex);
         free(surface->ctx.streamImages[i]);
@@ -2013,6 +2023,17 @@ EGLSurface wlEglCreatePlatformWindowSurfaceHook(EGLDisplay dpy,
         err = EGL_BAD_ALLOC;
         goto fail;
     }
+
+    if (!wlEglInitializeMutex(&surface->mutexFrameSync)) {
+        err = EGL_BAD_ALLOC;
+        goto fail;
+    }
+
+    if (pthread_cond_init(&surface->condFrameSync, NULL)) {
+        err = EGL_BAD_ALLOC;
+        goto fail;
+    }
+
 
     surface->wlEglDpy = display;
     surface->eglConfig = config;

include/wayland-eglsurface-internal.h
@@ -133,6 +133,13 @@ struct WlEglSurfaceRec {
     /* True when the EGL_PRESENT_OPAQUE_EXT surface attrib is set by the app */
     EGLBoolean presentOpaque;
 
+    /* This pair of mutex and conditional variable is used
+     * for sychronization between eglSwapBuffers() and damage
+     * thread on creating frame sync and waiting for it.
+     */
+    pthread_mutex_t mutexFrameSync;
+    pthread_cond_t condFrameSync;
+
     /* We want to delay the resizing of the window surface until the next
      * eglSwapBuffers(), so just set a resize flag.
      */

src/wayland-eglswap.c
@@ -99,6 +99,16 @@ EGLBoolean wlEglSwapBuffersWithDamageHook(EGLDisplay eglDisplay, EGLSurface eglS
             goto fail_locked;
         }
 
+        if (surface->ctx.useDamageThread) {
+            pthread_mutex_lock(&surface->mutexFrameSync);
+            // Wait for damage thread to submit the
+            // previous frame and generate frame sync
+            while (surface->ctx.framesProduced != surface->ctx.framesProcessed) {
+                pthread_cond_wait(&surface->condFrameSync, &surface->mutexFrameSync);
+            }
+            pthread_mutex_unlock(&surface->mutexFrameSync);
+        }
+
         wlEglWaitFrameSync(surface);
     }
 
@@ -127,10 +137,10 @@ EGLBoolean wlEglSwapBuffersWithDamageHook(EGLDisplay eglDisplay, EGLSurface eglS
         if (surface->ctx.useDamageThread) {
             surface->ctx.framesProduced++;
         } else {
+            wlEglCreateFrameSync(surface);
             res = wlEglSendDamageEvent(surface, surface->wlEventQueue);
         }
     }
-    wlEglCreateFrameSync(surface);
 
 done:
     // Release wlEglSurface lock.
@@ -257,6 +267,16 @@ EGLBoolean wlEglPrePresentExport(WlEglSurface *surface) {
     // Acquire wlEglSurface lock.
     pthread_mutex_lock(&surface->mutexLock);
 
+    if (surface->ctx.useDamageThread) {
+        pthread_mutex_lock(&surface->mutexFrameSync);
+        // Wait for damage thread to submit the
+        // previous frame and generate frame sync
+        while (surface->ctx.framesProduced != surface->ctx.framesProcessed) {
+            pthread_cond_wait(&surface->condFrameSync, &surface->mutexFrameSync);
+        }
+        pthread_mutex_unlock(&surface->mutexFrameSync);
+    }
+
     wlEglWaitFrameSync(surface);
 
     // Release wlEglSurface lock.
@@ -289,10 +309,9 @@ EGLBoolean wlEglPostPresentExport(WlEglSurface *surface) {
     if (surface->ctx.useDamageThread) {
         surface->ctx.framesProduced++;
     } else {
+        wlEglCreateFrameSync(surface);
         res = wlEglSendDamageEvent(surface, surface->wlEventQueue);
     }
-
-    wlEglCreateFrameSync(surface);
 
     // Release wlEglSurface lock.
     pthread_mutex_unlock(&surface->mutexLock);

commit f9c3a6d7a4dd0b30318d894103ceaf96aa6b524d

wayland-egl: Fix race condition in damage_thread
Reported by a static analysis tool:

  1. src/wayland-eglsurface.c:248:17: thread1_checks_field:
     Thread1 uses the value read from field "framesProcessed" in the
     condition "surface->ctx.framesFinished !=
     surface->ctx.framesProcessed".It sees that the condition is true.
     Control is switched to Thread2.
  2. src/wayland-eglsurface.c:248:17: thread2_checks_field:
     Thread2 uses the value read from field "framesProcessed" in the
     condition "surface->ctx.framesFinished !=
     surface->ctx.framesProcessed". It sees that the condition is true.
  3. src/wayland-eglsurface.c:255:17: thread2_acquires_lock:
     Thread2 acquires lock "WlEglSurfaceRec.mutexFrameSync".
  4. src/wayland-eglsurface.c:260:17: thread2_modifies_field:
     Thread2 sets "framesProcessed" to a new value. Note that this
     write can be reordered at runtime to occur before instructions
     that do not access this field within this locked region. After
     Thread2 leaves the critical section, control is switched back to
     Thread1.
  5. src/wayland-eglsurface.c:255:17: thread1_acquires_lock:
     Thread1 acquires lock "WlEglSurfaceRec.mutexFrameSync".
  6. src/wayland-eglsurface.c:260:17: thread1_overwrites_value_in_field:
     Thread1 sets "framesProcessed" to a new value. Now the two threads
     have an inconsistent view of "framesProcessed" and updates to
     fields correlated with "framesProcessed" may be lost.
  7. src/wayland-eglsurface.c:249:17: use_same_locks_for_read_and_modify:
     Guard the modification of "framesProcessed" and the read used to
     decide whether to modify "framesProcessed" with the same set of
     locks.
  #   258|
  #   259|                ok = wlEglSendDamageEvent(surface, queue);
  #   260|->              surface->ctx.framesProcessed++;
  #   261|
  #   262|                pthread_cond_signal(&surface->condFrameSync);

Fixes: 63e82b0 ("wayland-egl: avoid unnecessary commit")

src/wayland-eglsurface.c
@@ -339,6 +339,8 @@ damage_thread(void *args)
         // If not done, keep handling frames
         if (ok) {
 
+            pthread_mutex_lock(&surface->mutexFrameSync);
+
             // If there's an unprocessed frame ready, send damage event
             if (surface->ctx.framesFinished !=
                 surface->ctx.framesProcessed) {
@@ -347,16 +349,12 @@ damage_thread(void *args)
                                           surface->ctx.eglStream);
                 }
 
-                pthread_mutex_lock(&surface->mutexFrameSync);
-
                 wlEglCreateFrameSync(surface);
 
                 ok = wlEglSendDamageEvent(surface, queue);
                 surface->ctx.framesProcessed++;
 
                 pthread_cond_signal(&surface->condFrameSync);
-
-                pthread_mutex_unlock(&surface->mutexFrameSync);
             }
 
             // Otherwise, wait for sync to trigger
@@ -366,6 +364,8 @@ damage_thread(void *args)
                                                surface->ctx.damageThreadSync,
                                                0, EGL_FOREVER_KHR));
             }
+
+            pthread_mutex_unlock(&surface->mutexFrameSync);
         }
     }
 